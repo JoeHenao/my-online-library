f81bef23d1eb0459d7c2dcec85e7927a4de8339c
JoeHenao <48445206+JoeHenao@users.noreply.github.com>
2019-06-03 18:10:07 -0400

    Add files via upload

---
+++ my online library/php/inc/data.php
@@ -0,0 +1,166 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Diary of A Wimpy Kid",
+	"img" => "img/media/wimpy.jpg",
+    "genre" => "Fiction",
+    "format" => "Hardcover",
+    "year" => 2008,
+    "category" => "Books",
+    "authors" => [
+        "Jeff Kinney"
+    ],
+    "publisher" => "Amulet Books",
+    "isbn" => '0810993139'
+];
+$catalog[102] = [
+    "title" => "Of Mice and Men",
+    "img" => "img/media/men.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1993,
+    "category" => "Books",
+    "authors" => [
+        "John Steinback"
+    ],
+    "publisher" => "Penguin books",
+    "isbn" => '0140177396'
+];
+$catalog[103] = [
+    "title" => "Diary of a Wimpy Kid: Dog Days",
+    "img" => "img/media/wimp2.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 2010,
+    "category" => "Books",
+    "authors" => [
+        "Jeff Kinney"
+    ],
+    "publisher" => "Amulet Books",
+    "isbn" => ' 0810983915'
+];
+$catalog[104] = [
+    "title" => "Catcher in The Rye",
+    "img" => "img/media/catcher.jpg",
+    "genre" => "Fiction",
+    "format" => "Paperback",
+    "year" => 1951,
+    "category" => "Books",
+    "authors" => [
+        "J.D Sallinger"
+    ],
+    "publisher" => "Little Brown and Co.",
+    "isbn" => '0316769533'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Pulp Fiction",
+    "img" => "img/media/pulp.jpg",
+    "genre" => "Action",
+    "format" => "DVD/Stream",
+    "year" => 1993,
+    "category" => "Movies",
+    "director" => "Quentin Tarantino",
+    "writers" => [
+        "Quentin Tarantino",
+        "Quentin Tarantino"
+    ],
+    "stars" => [
+        "John Travolta",
+        "Samuel Jackson",
+        "Bruce Willis",
+    ]
+];
+$catalog[202] = [
+    "title" => "The 40-year old virgin",
+    "img" => "img/media/virg.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD/Streaming",
+    "year" => 2005,
+    "category" => "Movies",
+    "director" => "Judd Apatow",
+    "writers" => [
+        "Judd Apatow",
+				"Steve Carell"
+    ],
+    "stars" => [
+			"Steve Carrell",
+			"Seth Rogen",
+			"Paul Rudd"
+    ]
+];
+$catalog[203] = [
+    "title" => "The Man From U.N.C.L.E",
+    "img" => "img/media/wolf.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 2013,
+    "category" => "Movies",
+    "director" => "Martin Scorsese",
+    "writers" => [
+        "Terence Winter",
+        "Jordan Belfort"
+    ],
+    "stars" => [
+			"Leonardo Dicaprio",
+			"Jonah Hill",
+			"Margot Robbie"
+    ]
+];
+$catalog[204] = [
+    "title" => "Goodfellas",
+		"img" => "img/media/good.jpg",
+	  "genre" => "Drama, crime",
+	  "format" => "DVD/Streaming",
+	  "year" => 1990,
+	  "category" => "Movies",
+	  "director" => "Martin Scorsese",
+	  "writers" => [
+	      "Nicholas Pileggi"
+
+	  ],
+	  "stars" => [
+	      "Rober De Niro",
+	      "Ray Liotta",
+	      "Joe Pesci"
+	  ]
+	];
+//Music
+$catalog[301] = [
+	"title" => "Man on The Moon",
+	"img" => "img/media/cudi.jpg",
+	"genre" => "hip-hop",
+	"format" => "CD",
+	"year" => 2008,
+	"category" => "Music",
+	"artist" => "Kid Cudi"
+];
+
+$catalog[302] = [
+	"title" => "Undeniable",
+	"img" => "img/media/logic.jpg",
+	"genre" => "Rap/Hip-Hop",
+	"format" => "Streaming Services",
+	"year" => 2012,
+	"category" => "Music",
+	"artist" => "Logic"
+];
+$catalog[303] = [
+	"title" => "Blank Face",
+	"img" => "img/media/schoolboy.jpg",
+	"genre" => "Rap",
+	"format" => "CD, Streaming Services",
+	"year" => 2016,
+	"category" => "Music",
+	"artist" => "Schoolboy Q"
+];
+$catalog[304] = [
+	"title" => "Good kid, M.A.A.D City",
+	"img" => "img/media/kendrick.jpg",
+	"genre" => "Rap/Hip-Hop",
+	"format" => "CD, Streaming Services",
+	"year" => 2013,
+	"category" => "Music",
+	"artist" => "Kendrick Lamar"
+];
